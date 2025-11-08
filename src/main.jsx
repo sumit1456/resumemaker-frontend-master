80df899fc601df3b5ecab47736508001a1119891
sumit1456 <sumithatekar067@gmail.com>
2025-11-08 11:35:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
  
   <BrowserRouter>
 
-     <Provider store={store}>
+     <Provider >
         <App />
     </Provider>
   
