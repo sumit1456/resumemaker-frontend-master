c8c6914ca10b64a2d1a5aa07d1b540c170117d04
sumit1456 <sumithatekar067@gmail.com>
2025-11-08 11:42:17 +0530

    import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "../../redux/store.js";
+import store from './redux/store.js';
 
 
 
