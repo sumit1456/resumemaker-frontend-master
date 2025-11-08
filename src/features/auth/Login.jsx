9ed60a155ef571a13e564e5eebe080db50d30ab8
sumit1456 <sumithatekar067@gmail.com>
2025-11-08 23:41:22 +0530

    urlchange

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -6,8 +6,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { logInUser } from "../../redux/store.js";
 import { GoogleLogin } from '@react-oauth/google';
 
-const API_BASE_URL2 = 'https://resumemaker-1.onrender.com';
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://resumemaker-1.onrender.com';
+const API_BASE_URL2 = 'http://localhost:8080';
 
 export default function Login({ setUserId }) {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
